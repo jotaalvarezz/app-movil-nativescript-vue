d322f549da8e4bf78e12b638e9b3ff0078cb102b
jotazz25 <jalvarez@tecbaco.com>
2023-06-09 16:44:07 -0500

    configurando sqLite

--- app/components/scanner/ScanCode.vue
+++ app/components/scanner/ScanCode.vue
@@ -1,6 +1,6 @@
 <template>
     <FlexboxLayout flexDirection="column" backgroundColor="#3c495e">
-        <TextField v-model="pallet" paddingLeft="10" fontSize="30" />
+        <TextField v-model="pallet" paddingLeft="10" fontSize="30" @textChange="insertData(pallet)" />
         <!-- <Button backgroundColor="#43b883" @tap="openScan">Open Scan</Button> -->
         <Button row="2" backgroundColor="#43b883" text="back camera, with flip"
             @tap="doScanWithBackCameraWithFlip"></Button>
@@ -9,13 +9,17 @@
             preferFrontCamera="false" :pause="pause" @scanResult="onScanResult" v-if="activate">
         </BarcodeScanner>
         <Button text="Crear tabla" @tap="createTable" />
+        <Button text="Insertar datos" @tap="insertData" />
+        <Button text="Mostrar datos" @tap="getData" />
+        <Button text="open1" @tap="open1" />
+        <Button text="open" @tap="open" />
     </FlexboxLayout>
 </template>
 
 <script>
 import { isIOS } from "@nativescript/core";
 import { BarcodeScanner } from "nativescript-barcodescanner";
-const { createTable } = require('../../sqlite/database.js');
+const { createTable, insertData, allData, openDatabase } = require('../../sqlite/database.js');
 
 export default {
     data() {
@@ -28,12 +32,39 @@ export default {
     },
 
     methods: {
+        async open(){
+            try {
+                let open = await openDatabase()
+                console.log("open ",open)
+            } catch (error) {
+                console.log(error)
+            }
+        },
+
         async createTable() {
             try {
                 let table = await createTable();
                 console.log('Tabla creada correctamente ', table);
             } catch (error) {
                 console.error('Error al crear la tabla:', error);
+            }
+        },
+
+        async insertData(pallet) {
+            try {
+                const id = await insertData(pallet);
+                console.log('Datos insertados correctamente. ID:', id);
+            } catch (error) {
+                consol.log('insert error ',error)
+            }
+        },
+
+        async getData() {
+            try {
+                const res = await allData()
+                console.log('data: ',res)
+            } catch (error) {
+                console.log(error)
             }
         },
 
